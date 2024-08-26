93b14adb91f338b92172e10cae879af4f5367e56
Genesisrocio12 <166847477+Genesisrocio12@users.noreply.github.com>
2024-08-26 12:28:18 -0500

    Evaluacion Final-Parte practica-Tito Genesis

--- utils/defaults.php
+++ utils/defaults.php
@@ -1,11 +1,20 @@
 <?php
+// Verifico si la función 'view' no existe para evitar redefinirla
 if (!function_exists("view")) {
+
+    // Defino la función 'view' que carga una vista con parámetros
     function view($nombreVista, $params)
     {
+        // Recorro los parámetros y creo variables dinámicas con sus nombres y valores
         foreach ($params as $key => $param) {
             $$key = $param;
         }
+
+        // Divido el nombre de la vista en partes utilizando el punto como separador
         $vista = explode('.', $nombreVista);
+
+        // Incluyo el archivo PHP correspondiente a la vista desde la carpeta 'views'
         include_once "./views/{$vista[0]}/{$vista[1]}.php";
     }
 }
+?>
