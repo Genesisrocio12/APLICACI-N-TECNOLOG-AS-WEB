9defa51a119994ae6ff14abc132949a832c4a480
Genesisrocio12 <166847477+Genesisrocio12@users.noreply.github.com>
2024-08-26 12:21:05 -0500

    Evaluacion Final-Parte practica-Tito Genesis

--- models/DB.php
+++ models/DB.php
@@ -1,7 +1,13 @@
 <?php
 class DB extends PDO {
+    // Constructor de la clase DB, que extiende la clase PDO para manejar la base de datos
     public function __construct() {
+        // Defino el Data Source Name (DSN) para conectarme a la base de datos MySQL
+        // Especifico el host como 'localhost' y la base de datos como 'proyecto-libros'
         $dsn = 'mysql:host=localhost;dbname=proyecto-libros';
+        
+        // Llamo al constructor de la clase PDO, pasando el DSN y las credenciales (usuario 'root' y contraseña vacía)
         parent::__construct($dsn, 'root', '');
     }
 }
+?>
